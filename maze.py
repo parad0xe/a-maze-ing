9cb766d0ab53c84e0d0ea918848905f4c178083a
Leonard Decavele <ldecavel@z2r10p5.42lyon.fr>
2026-01-07 16:10:42 +0100

    wip: setters and getters

--- maze.py
+++ maze.py
@@ -1,3 +1,10 @@
+from error import SetterError
+
+
+MINIMUM_EDGE_SIZE: int = 1
+MAXIMUM_EDGE_SIZE: int = 500
+
+
 class Maze:
 
     def __init__(self) -> None:
@@ -9,8 +16,92 @@ class Maze:
         self._output_file: str | None = None
         self._perfect: bool | None = None
 
+    @property
+    def map(self) -> list[list[int]] | None:
+        return self._map
+
     @map.setter
     def map(self, value: list[list[int]]) -> None:
         self._map = value
 
-    # setters and getters
+    @property
+    def width(self) -> int:
+        return self._width
+
+    @width.setter
+    def width(self, value: str) -> None:
+        try:
+            width: int = int(value)
+            if not MINIMUM_EDGE_SIZE <= width <= MAXIMUM_EDGE_SIZE:
+                raise SetterError(f"invalid width: {value}")
+            self._width = width
+        except ValueError:
+            raise SetterError(f"invalid width: {value}")
+
+    @property
+    def height(self) -> int:
+        return self._height
+
+    @height.setter
+    def height(self, value: str) -> None:
+        try:
+            height: int = int(value)
+            if not MINIMUM_EDGE_SIZE <= height <= MAXIMUM_EDGE_SIZE:
+                raise SetterError(f"invalid height: {value}")
+            self._height = height
+        except ValueError:
+            raise SetterError(f"invalid height: {value}")
+
+    @property
+    def entry(self) -> tuple[int, int] | None:
+        return self._entry
+
+    @entry.setter
+    def entry(self, value: str) -> None:
+        try:
+            pos: list[int] = map(int, value.split(","))
+            if len(pos != 2):
+                raise SetterError(f"invalid entry: {value}")
+            x, y = pos
+            self._entry = (x, y)
+        except ValueError:
+            raise SetterError(f"invalid entry: {value}")
+
+    @property
+    def exit(self) -> tuple[int, int] | None:
+        return self._exit
+
+    @exit.setter
+    def exit(self, value: str) -> None:
+        try:
+            pos = map(int, value.split(","))
+            if len(pos != 2):
+                raise SetterError(f"invalid entry: {value}")
+            x, y = pos
+            self._exit = (x, y)
+        except ValueError:
+            raise SetterError(f"invalid exit: {value}")
+
+    @property
+    def output_file(self) -> str | None:
+        return self._output_file
+
+    @output_file.setter
+    def output_file(self, value: str) -> None:
+        v = value.strip()
+        if not v:
+            raise SetterError("invalid output_file: cannot be empty")
+        self._output_file = v
+
+    @property
+    def perfect(self) -> bool | None:
+        return self._perfect
+
+    @perfect.setter
+    def perfect(self, value: str) -> None:
+        if value == "True":
+            self._perfect = True
+        elif value == "False":
+            self._perfect = False
+        else:
+            raise SetterError(f"invalid perfect: {value}")
--- parsing.py
+++ parsing.py
@@ -2,13 +2,26 @@ from .maze import Maze
 from .errors import ParseError, SetterError
 
 
+def is_valid_pos(maze: Maze, pos: tuple[int, int]) -> bool:
+    x, y = pos
+    if not 0 <= y < maze.height:
+        return False
+    if not 0 <= x < maze.width:
+        return False
+
+
 def parse(maze: Maze, input_file: str) -> None:
     with open("r", input_file) as f:
         for line in f:
             if line.strip().startswith("#"):
                 continue
             key, value = line.split("=")
-            key = key.lower()
+            if not key:
+                raise ParseError("invalid key: cannot be empty")
+            if not value:
+                raise ParseError("invalid value: cannot be empty")
+            key = key.strip().lower()
+            value = value.strip()
             if hasattr(maze, key):
                 try:
                     setattr(maze, key, value)
@@ -16,3 +29,7 @@ def parse(maze: Maze, input_file: str) -> None:
                     raise ParseError(f"invalid value: {e}")
             else:
                 raise ParseError("invalid key")
+        if not is_valid_pos(maze, maze.entry):
+            raise ParseError("invalid entry")
+        if not is_valid_pos(maze, maze.exit):
+            raise ParseError("invalid exit")
