8b4a854d0cbc3b0fbeeb8cfd807d11871f955ce0
Leonard Decavele <ldecavel@z2r9p6.42lyon.fr>
2026-01-21 17:51:53 +0100

    wip: render

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -5,6 +5,7 @@ import sys
 from maze import Maze, generate, load, render, solve
 from maze.error import ErrCode, ParseError
 from maze.renderer import GraphicalRenderer
+from typing import Any
 
 logger = logging.getLogger(__name__)
 
@@ -16,15 +17,46 @@ def main(argc: int, argv: list[str]) -> int:
         )
         return ErrCode.INPUT_ERROR
 
-    try:
-        maze: Maze = load(argv[1])
-    except ParseError as e:
-        logger.error(f"parse error: {e}")
-        return ErrCode.PARSE_ERROR
+    #try:
+    #    maze: Maze = load(argv[1])
+    #except ParseError as e:
+    #    logger.error(f"parse error: {e}")
+    #    return ErrCode.PARSE_ERROR
 
-    generate(maze)
-    solve(maze)
-    render(maze, GraphicalRenderer)
+    maze = Maze()
+    maze.map = [
+      [9, 5, 1, 5, 3, 9, 1, 5, 3, 9, 5, 5, 1, 7, 9, 5, 1, 5, 1, 1, 5, 1, 1, 5, 3],
+      [14, 11, 10, 11, 10, 14, 8, 1, 2, 8, 5, 3, 12, 1, 4, 1, 2, 11, 10, 8, 1, 2, 8, 1, 2],
+      [9, 6, 10, 8, 4, 1, 6, 10, 8, 4, 5, 4, 5, 4, 1, 2, 10, 12, 4, 2, 8, 2, 12, 2, 10],
+      [12, 3, 10, 8, 3, 8, 1, 6, 10, 9, 3, 9, 5, 3, 8, 4, 4, 5, 3, 10, 8, 2, 0, 0, 2],
+      [9, 6, 8, 4, 2, 10, 8, 5, 2, 10, 12, 0, 7, 10, 10, 13, 1, 3, 10, 8, 2, 8, 3, 12, 2],
+      [12, 1, 2, 9, 6, 12, 4, 3, 10, 10, 11, 8, 3, 10, 10, 9, 2, 10, 10, 8, 6, 8, 6, 11, 10],
+      [9, 2, 14, 8, 5, 3, 9, 6, 8, 4, 2, 8, 4, 4, 4, 6, 8, 2, 10, 12, 1, 2, 9, 0, 2],
+      [10, 12, 3, 8, 1, 4, 4, 5, 2, 15, 10, 8, 3, 15, 15, 15, 8, 2, 12, 5, 2, 12, 4, 2, 10],
+      [8, 5, 6, 8, 4, 1, 1, 7, 10, 15, 12, 6, 8, 5, 7, 15, 10, 12, 1, 3, 8, 3, 13, 0, 6],
+      [12, 5, 3, 10, 13, 0, 4, 3, 10, 15, 15, 15, 10, 15, 15, 15, 8, 5, 6, 10, 10, 8, 1, 4, 3],
+      [9, 1, 4, 4, 1, 2, 9, 4, 2, 9, 7, 15, 10, 15, 13, 5, 0, 1, 1, 4, 2, 12, 6, 11, 10],
+      [10, 10, 9, 1, 2, 10, 12, 3, 8, 4, 3, 15, 10, 15, 15, 15, 8, 2, 8, 5, 6, 13, 5, 2, 10],
+      [8, 4, 2, 10, 8, 6, 9, 2, 10, 9, 2, 11, 8, 5, 1, 7, 12, 4, 4, 5, 1, 5, 5, 2, 10],
+      [8, 1, 6, 10, 12, 3, 8, 4, 4, 6, 8, 2, 8, 5, 2, 9, 3, 9, 1, 7, 4, 9, 5, 4, 2],
+      [12, 4, 1, 6, 9, 2, 8, 5, 1, 3, 12, 4, 4, 3, 10, 8, 2, 8, 4, 5, 6, 12, 3, 11, 10],
+      [9, 1, 4, 1, 6, 10, 10, 9, 2, 12, 3, 9, 3, 10, 8, 2, 8, 0, 1, 5, 5, 3, 10, 10, 10],
+      [10, 8, 1, 2, 9, 2, 10, 10, 8, 1, 4, 6, 8, 2, 12, 6, 10, 8, 6, 9, 3, 12, 6, 10, 10],
+      [10, 8, 4, 4, 2, 12, 6, 12, 2, 12, 1, 1, 6, 8, 5, 5, 2, 12, 1, 6, 10, 9, 5, 4, 2],
+      [8, 6, 9, 5, 6, 9, 5, 1, 6, 9, 2, 12, 1, 4, 5, 5, 4, 1, 6, 9, 2, 8, 5, 5, 2],
+      [12, 5, 4, 5, 5, 4, 5, 4, 5, 6, 12, 5, 4, 5, 5, 5, 5, 4, 5, 4, 4, 4, 5, 5, 6],
+    ]
+    maze.width = "25" # offset by one
+    maze.height = "20" # offset by one
+    maze.entry = "1,1"
+    maze.exit = "19,14"
+    maze.output_file = "test.txt"
+    maze.perfect = "False"
+
+    #generate(maze)
+    #solve(maze)
+    mlx_renderer = GraphicalRenderer(maze, 60)
+    render(maze, mlx_renderer)
 
     #  print function
     while True:
--- maze/renderer.py
+++ maze/renderer.py
@@ -1,12 +1,26 @@
 from typing import Protocol
 
 from .maze import Maze
+from enum import IntEnum
+from typing import Any
+from mlx import Mlx
+
 """
 examples:
 
 render(TerminalRenderer, maze)
 render(GraphicalRenderer, maze)
 """
+
+
+class Cell(IntEnum):
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
+    CURSOR = 4
+    SEEK = 5
+    PATH = 6
 
 
 class Renderer(Protocol):
@@ -23,9 +37,106 @@ class TerminalRenderer:
 
 class GraphicalRenderer:
 
+    def __init__(self, maze: Maze, scale: int) -> None:
+        self._maze = maze
+        self._mlx: Mlx = Mlx()
+        self._mlx_ptr: Any = self._mlx.mlx_init()
+        self._scale = scale
+        self._image = self._mlx.mlx_new_image(
+            self._mlx_ptr,
+            maze.width * self._scale,
+            maze.height * self._scale,
+        )
+        self._image_data: tuple[Any, ...] = self._mlx.mlx_get_data_addr(
+            self._image
+        )
+        self._window = self._mlx.mlx_new_window(
+            self._mlx_ptr,
+            maze.width * self._scale,
+            maze.height * self._scale,
+            "A-Maze-ing"
+        )
+
+    def put_pixel_img(self, x: int, y: int, color: int) -> None:
+        idx = (y * self._image_data[2] + x * (self._image_data[1] // 8))
+        image = self._image_data[0]
+        image[idx + 0] = color & 0xFF
+        image[idx + 1] = (color >> 8) & 0xFF
+        image[idx + 2] = (color >> 16) & 0xFF
+        image[idx + 3] = (color >> 24) & 0xFF
+
+    def fill_cell(self, cx: int, cy: int, color: int, cell: int) -> None:
+        bpp = self._image_data[1] // 8
+        line = self._image_data[2]
+        img = self._image_data[0]
+
+        x0 = cx * self._scale
+        y0 = cy * self._scale
+
+        b0 = color & 0xFF
+        b1 = (color >> 8) & 0xFF
+        b2 = (color >> 16) & 0xFF
+        b3 = (color >> 24) & 0xFF
+
+        x_start: int = 0
+        x_end: int = self._scale
+        y_start: int = 0
+        y_end: int = self._scale
+        if cell & 0x1:
+            y_start = int(self._scale * 0.2)
+        if cell & 0x2:
+            x_end = int(self._scale * 0.8)
+        if cell & 0x4:
+            y_end = int(self._scale * 0.8)
+        if cell & 0x8:
+            x_start = int(self._scale * 0.2)
+
+        for dy in range(self._scale):
+            y = y0 + dy
+            row = y * line + x0 * bpp
+            for x in range(self._scale):
+                if x_start < x < x_end and y_start < dy < y_end:
+                    img[row + 0] = 255
+                    img[row + 1] = 0
+                    img[row + 2] = 0
+                    img[row + 3] = 140
+                    row += bpp
+                else:
+                    img[row + 0] = b0
+                    img[row + 1] = b1
+                    img[row + 2] = b2
+                    img[row + 3] = b3
+                    row += bpp
+
+
+    def update(self) -> None:
+        m = self._maze.map
+        h = self._maze.height
+        w = self._maze.width
+
+        for cy in range(h):
+            for cx in range(w):
+                color = 0xFF000077
+                self.fill_cell(cx, cy, color, m[cy][cx])
+
     def render(self) -> None:
-        raise NotImplementedError("graphical render method not implemented")
+        def tick(_): # main thread
+            self.update()
+            self._mlx.mlx_put_image_to_window(
+                self._mlx_ptr,
+                self._window,
+                self._image,
+                0,
+                0
+            )
+            input("t")
+
+        self._mlx.mlx_loop_hook(
+            self._mlx_ptr, tick, None
+        )
+        self._mlx.mlx_loop(self._mlx_ptr)
 
 
-def render(maze: Maze, renderer: type[Renderer]) -> None:
-    raise NotImplementedError("render method not implemented")
+def render(maze: Maze, renderer: Renderer) -> None:
+    renderer.render()
+    #raise NotImplementedError("render method not implemented")
