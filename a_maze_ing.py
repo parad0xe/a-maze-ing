8e42369c37847b0be910ad9f109d5bf40b7e5ffa
Nathan Lallemand <nlallema@z3r11p6.42lyon.fr>
2026-02-03 11:13:54 +0100

    refacto: error handling, main params

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -115,47 +115,33 @@ def update(
             sys.exit(3)
 
 
-def main(argc: int, argv: list[str]) -> None:
+def main() -> None:
     logging.config.dictConfig(LOGGING_CONFIG)
 
-    if argc < 2:
+    if len(sys.argv) < 2:
         logger.error(
             f"Usage: python3 {os.path.basename(__file__)} <config_file>"
         )
         sys.exit(1)
 
-    try:
-        maze: Maze = load(argv[1])
-    except ValidationError as e:
-        log_validation_errors(e)
-        sys.exit(2)
-    except Exception as e:
-        logger.error(f"{type(e).__name__}: {e}")
-        sys.exit(2)
+    maze: Maze = load(sys.argv[1])
 
     random.seed(maze.seed)
-    try:
-        engine = GraphicalEngine(
-            maze=maze,
-            config=EngineConfig(wall_size=2, cell_size=40),
-            palette=Palette(
-                empty=0x000000FF,
-                wall=0x0000AAFF,
-                unreachable=0xAAAAAAFF,
-                cursor=0x00FF00FF,
-                path=0xFF000055,
-                seek=0x00FF0033,
-                seek_premium=0xFF00FFAA,
-                entry=0xFFFFFFFF,
-                exit=0xFFFFFFFF,
-            ),
-        )
-    except ValidationError as e:
-        log_validation_errors(e)
-        sys.exit(2)
-    except Exception as e:
-        logger.error(f"{type(e).__name__}: {e}")
-        sys.exit(2)
+    engine = GraphicalEngine(
+        maze=maze,
+        config=EngineConfig(wall_size=2, cell_size=40),
+        palette=Palette(
+            empty=0x000000FF,
+            wall=0x0000AAFF,
+            unreachable=0xAAAAAAFF,
+            cursor=0x00FF00FF,
+            path=0xFF000055,
+            seek=0x00FF0033,
+            seek_premium=0xFF00FFAA,
+            entry=0xFFFFFFFF,
+            exit=0xFFFFFFFF,
+        ),
+    )
 
     print(
         "=== A-Maze-Ing ===",
@@ -169,22 +155,26 @@ def main(argc: int, argv: list[str]) -> None:
         flush=True,
     )
 
+    engine.loop(
+        update,
+        keypress=keypress,
+        args=LoopContext(
+            generator=maze.iter_generate(),
+            solver=maze.iter_solve(),
+        ),
+    )
+
+
+if __name__ == "__main__":
     try:
-        engine.loop(
-            update,
-            keypress=keypress,
-            args=LoopContext(
-                generator=maze.iter_generate(),
-                solver=maze.iter_solve(),
-            ),
-        )
+        main()
     except ValidationError as e:
-        log_validation_errors(e)
+        for error in e.errors():
+            field = " -> ".join(str(item) for item in error["loc"])
+            message = error["msg"]
+            logger.error(f"{type(e).__name__}: {message} ({field}) ")
+            log_validation_errors(e)
         sys.exit(2)
     except Exception as e:
         logger.error(f"{type(e).__name__}: {e}")
         sys.exit(2)
-
-
-if __name__ == "__main__":
-    main(len(sys.argv), sys.argv)
