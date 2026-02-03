f988e0a9cc2c8f6b02f4b6fea167b93b4a0f9334
Leonard Decavele <ldecavel@z3r10p5.42lyon.fr>
2026-02-03 16:40:53 +0100

    edit: docstrings

--- srcs/errors.py
+++ srcs/errors.py
@@ -2,10 +2,22 @@ from enum import IntEnum
 
 
 class ParseError(Exception):
+    """
+    This exception signals a configuration parsing failure.
+    """
     pass
 
 
 class ExitCode(IntEnum):
+    """
+    This enum defines process exit codes for common failure cases.
+
+    Attributes:
+        VALIDATION_ERROR (int): Validation failed for the loaded configuration.
+        FILE_ERROR (int): File IO or encoding error occurred during export.
+        ARGUMENTS_ERROR (int): Invalid or missing command line arguments.
+        UNEXPECTED_ERROR (int): Any other unhandled error occurred.
+    """
     VALIDATION_ERROR = 1
     FILE_ERROR = 2
     ARGUMENTS_ERROR = 3
